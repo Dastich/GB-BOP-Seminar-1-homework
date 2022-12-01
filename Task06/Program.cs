dd3534dbf2ff934c47ac8d82db815af712219fed
Maksim <roker777799@gmail.com>
2022-12-01 22:37:37 +0300

    complete 6th task

---
+++ Task06/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число для проверки на четность: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+Console.WriteLine("Число "+number+" четное");
+else
+Console.WriteLine("Число "+number+" нечетное");
+
