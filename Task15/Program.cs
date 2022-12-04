e278a992c530d9cfdd2f61d1df029c1e7f0b8d09
Maksim <roker777799@gmail.com>
2022-12-05 00:07:40 +0300

    complete 15th task

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите день недели: ");
+int day =Convert.ToInt32(Console.ReadLine());
+if(day<1||day>7);
+{
+    while(day<1||day>7)
+    {
+    Console.Write("Введенное число некорректно, введите верный день недели: ");
+    day=Convert.ToInt32(Console.ReadLine());
+    }
+}
+if(day>=1&&day<=5)
+Console.WriteLine("Сегодня будний день :(");
+else
+Console.WriteLine("Сегодня выходной :)");
