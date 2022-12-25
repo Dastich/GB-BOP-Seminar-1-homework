7e18e766a1c7d57021b1e3d31f3686288e7543ea
Maksim <roker777799@gmail.com>
2022-12-25 21:56:17 +0300

    complete 60th task

---
+++ task60/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// массив размером 2 x 2 x 2
+
+// 12(0,0,0) 22(0,0,1)
+
+// 45(1,0,0) 53(1,0,1)
+
+void PrintValueAndIndex(int[,,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            for(int k=0;k<array.GetLength(2);k++)
+            {
+                Console.Write($"{array[i,k,k]}({i}{j}{k}) ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
+
+
+int[,,] array3D={
+    {
+         {1,2},
+         {3,4},
+    },
+    {
+        {5,6},
+        {7,8},
+
+    }
+};
+PrintValueAndIndex(array3D);
